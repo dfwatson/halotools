e0a39fd96a7bf8c17c622680ddf943a76f4b6f68
dfwatson <dfwatson@gmail.com>
2014-08-27 14:26:42 -0600

    Update mcmc.py
    just a little bit more info

--- halotools/mcmc.py
+++ halotools/mcmc.py
@@ -1,24 +1,26 @@
 """
 
 This module contains the classes and methods used to perform the generic Metropolis-Hastings 
-algorithm for doing an MCMC over a number of observables
+algorithm for doing an MCMC over a variety of observables
 
 """
 
 
-__all__ = ['HOD_Model','Zheng07_HOD_Model','Toy_HOD_Model','Assembly_Biased_HOD_Model',
-'HOD_Quenching_Model','vdB03_Quenching_Model','Assembly_Biased_HOD_Quenching_Model',
-'Satcen_Correlation_Polynomial_HOD_Model','Polynomial_Assembly_Biased_HOD_Model',
-'cumulative_NFW_PDF','anatoly_concentration','solve_for_polynomial_coefficients']
-#from __future__ import (absolute_import, division, print_function,
-#                        unicode_literals)
+__all__ = ['Mmin','M1','M0','alpha','sigma']
+
 
 import numpy as np
-from scipy.special import erf
-from scipy.stats import poisson
+from scipy.signal import gaussian
 import defaults
 
 from astropy.extern import six
 from abc import ABCMeta, abstractmethod, abstractproperty
 import warnings
+
+"""""
+
+will want function that passes HOD parameters to each observable
+
+"""""
+
 
