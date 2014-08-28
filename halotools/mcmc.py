24fe589cede12e415cca556cb245dfd793521376
dfwatson <dfwatson@gmail.com>
2014-08-28 12:12:24 -0600

    Update mcmc.py
    slowly putting in MCMC parameter stepping functions

--- halotools/mcmc.py
+++ halotools/mcmc.py
@@ -6,21 +6,50 @@ algorithm for doing an MCMC over a variety of observables
 """
 
 
-__all__ = ['Mmin','M1','M0','alpha','sigma']
-
-
 import numpy as np
 from scipy.signal import gaussian
 import defaults
 
-from astropy.extern import six
-from abc import ABCMeta, abstractmethod, abstractproperty
-import warnings
 
 """""
 
 will want function that passes HOD parameters to each observable
 
 """""
+
+class MCMC(sampler):
+  """ Container class for the MCMC algorithm.    
+    
+  """
+  
+  def MCMC_stepping(self,step_size):  
+  
+  #choose a seed value such that a given chain can be reproduce
+  #if one use "None" then that will just use the current system
+  #time and the chain will not be reproducable
+
+    seed = -3004
+    np.random.seed(self.seed)
+  
+    HOD_parameter_names = (['Mmin','M1','M0','alpha','sigma'])
+
+    step_Mmin = scipy.signal.gaussian(self.seed)*step_size    
+    step_M1 = scipy.signal.gaussian(self.seed)*step_size  
+    step_M0 = scipy.signal.gaussian(self.seed)*step_size   
+    step_alpha = scipy.signal.gaussian(self.seed)*step_size   
+    step_sigma = scipy.signal.gaussian(self.seed)*step_size   
+    
+    new_Mmin = Mmin + step_Mmin
+    new_M1 = M1 + step_M1
+    new_M0 = M0 + step_M0
+    new_alpha = alpha + step_alpha
+    new_sigma = sigma + step_sigma
+
+  # will now want to pass these new HOD values to the various
+  # observable calculations, then compute chi-squared from the
+  # combination of model comparisons to each observable 
+
+
+
 
 
